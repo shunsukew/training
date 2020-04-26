4956c08f2133d32091fe57018a071555053d22f3
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2020-04-26 18:06:05 +0900

    file sysytem

---
+++ file_system/file/src/main.rs
@@ -0,0 +1,32 @@
+extern crate memmap;
+
+use memmap::Mmap;
+
+use std::fs::File;
+use std::io::{Write, BufReader, BufRead, Error};
+
+fn main() -> Result<(), Error> {
+    let path = "lines.txt";
+
+    let mut output = File::create(path)?;
+    write!(output, "Rust\n💖\nFun")?;
+
+    let input = File::open(path)?;
+    let buffered = BufReader::new(input);
+
+    for line in buffered.lines() {
+        println!("{}", line?);
+    }
+
+    let file = File::open("content.txt")?;
+    let map = unsafe { Mmap::map(&file)? };
+
+    let random_indexes = [0, 1, 2, 19, 22, 10, 11, 29];
+    assert_eq!(&map[3..13], b"hovercraft");
+    let random_bytes: Vec<u8> = random_indexes.iter()
+        .map(|&idx| map[idx])
+        .collect();
+    assert_eq!(&random_bytes[..], b"My loaf!");
+
+    Ok(())
+}
